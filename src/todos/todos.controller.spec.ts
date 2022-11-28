085e2c9884c6560e0c9b31510431ca54832861d6
AIT-BOUHOU <ahmed.ait.bouhou@nova-technology.fr>
2022-11-29 00:22:37 +0100

    add testing jest

--- src/todos/todos.controller.spec.ts
+++ src/todos/todos.controller.spec.ts
@@ -1,6 +1,10 @@
 import { TodosController } from './todos.controller';
 import { TodosService } from './todos.service';
 import { Test } from '@nestjs/testing';
+import { IToDo } from './interface/ITodo.interface';
+import { HttpStatus } from '@nestjs/common';
+import { ToDosModule } from './todos.module';
+import { CreateToDodto } from './dto/create-todo.dto';
 
 describe('TodosController', () => {
   let todosController: TodosController;
@@ -9,19 +13,80 @@ describe('TodosController', () => {
   beforeEach(async () => {
     const moduleRef = await Test.createTestingModule({
       controllers: [TodosController],
-      providers: [TodosService],
+      providers: [
+        {
+          provide: TodosService,
+          useValue: {
+            findAllToDos: jest.fn(),
+            findOne: jest.fn(),
+            createTodo: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     todosService = moduleRef.get<TodosService>(TodosService);
     todosController = moduleRef.get<TodosController>(TodosController);
   });
 
-  describe('findAll', () => {
-    it('should return an array of cats', async () => {
-      const result = ['test'];
-//jest.spyOn(todosService, 'findAllToDos').mockImplementation(() => result);
+  it('TodosController - should be defined', () => {
+    expect(todosController).toBeDefined();
+  });
 
-     /// expect(await todosController.findAll()).toBe(result);
+  describe('findAll', () => {
+    const response = {
+      json: (body?: any) => { },
+      status: (code: number) => HttpStatus.OK,
+    };
+
+    it('should return an array of todos', async () => {
+      const result: IToDo[] = [];
+
+      jest
+        .spyOn(todosService, 'findAllToDos')
+        .mockImplementation(
+          async (): Promise<IToDo[]> => Promise.resolve(result),
+        );
+
+      expect(await todosController.findAll(response)).toBe(result);
+    });
+  });
+
+  describe('findOne', () => {
+    const response = {
+      json: (body?: any) => { },
+      status: (code: number) => HttpStatus.OK,
+    };
+
+    it('should return an todo', async () => {
+      const result: IToDo = null;
+
+      jest
+        .spyOn(todosService, 'findOne')
+        .mockImplementation(
+          async (): Promise<IToDo> => Promise.resolve(result),
+        );
+
+      expect(await todosController.findOne(response, 'XXXXXXX')).toBe(result);
+    });
+  });
+
+  describe('create', () => {
+    const response = {
+      json: (body?: any) => { },
+      status: (code: number) => HttpStatus.OK,
+    };
+
+    it('should return an todo after create it', async () => {
+      const result: IToDo = null;
+      const createDto = new CreateToDodto();
+      jest
+        .spyOn(todosService, 'createTodo')
+        .mockImplementation(
+          async (): Promise<IToDo> => Promise.resolve(result),
+        );
+
+      expect(await todosController.create(response, createDto)).toBe(result);
     });
   });
 });
--- src/todos/todos.service.spec.ts
+++ src/todos/todos.service.spec.ts
@@ -1,18 +1,38 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { of } from 'rxjs';
+import { CreateToDodto } from './dto/create-todo.dto';
 import { TodosService } from './todos.service';
 
 describe('TodosService', () => {
-  let service: TodosService;
+  let todosService: TodosService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TodosService],
+      providers: [{
+        provide: TodosService,
+        useValue: {},
+        },
+      ],
     }).compile();
 
-    service = module.get<TodosService>(TodosService);
+    todosService = module.get<TodosService>(TodosService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('TodosService - should be defined', () => {
+    expect(todosService).toBeDefined();
+  });
+
+  describe('findAll', () => {
+    it('should return an array of todos', async () => {
+      expect(await todosService.findAllToDos).toBe(undefined);
+    });
+  });
+
+  describe('createTodo', () => {
+    it('should create and return an todo', async () => {
+      const createToDodto = new CreateToDodto();
+      expect(await todosService.createTodo).toBe(createToDodto);
+    });
   });
 });
+
